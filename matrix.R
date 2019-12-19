a61f1e9f9d850d5cd00fac47eb993d804fabc254
Pavan Nallamalli <pavan.nallamalli@gmail.com>
2019-12-19 15:21:49 +0530

    Initial commit for this proj dir R Programming

---
+++ matrix.R
@@ -0,0 +1,3 @@
+matrix(c(1,2,3,4,5,6,7,8),nrow = 2,ncol = 4)
+matrix(c(1,2,3,4),nrow=2, ncol=2)
+2+2
---
+++ quadraticOuputs.R
@@ -0,0 +1,10 @@
+# store the values of a b and c to compute the quadratic expression
+a=2
+b=-1
+c=-4
+# store the values of computation in each of the variables
+solution_1 = (-b + sqrt(b^2 - 4*a*c))/(2*a)
+solution_2 = (-b - sqrt(b^2 - 4*a*c))/(2*a)
+#print the values of each of these
+print(solution_1)
+print(solution_2)
